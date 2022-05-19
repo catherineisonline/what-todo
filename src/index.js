b5d17ff29b214bb7287b0bfa59460373afff92a2
Ekaterine (Catherine) Mitagvaria <63928803+catherineisonline@users.noreply.github.com>
2022-05-19 21:56:36 +0400

    adding gh-pages

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 //Styles
 import "../src/assets/index.css";
-import "../src/assets/responsive.css"
+import "../src/assets/responsive.css";
 import App from "./App";
 //Redux
 import store from "./redux/store";
@@ -10,9 +12,13 @@ import { Provider } from "react-redux";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <Router>
+      <Provider store={store}>
+        <Routes>
+          <Route path="/what-todo" element={<App />} />
+        </Routes>
+      </Provider>
+    </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
